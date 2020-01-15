f2ad16b21cbe76b37c7a8ef68c5020771a37b61e
arpine1234 <arpine.hoveyan.g@tumo.org>
2020-01-15 16:40:41 +0400

    Hello

---
+++ script.js
@@ -0,0 +1,37 @@
+ var a =prompt("i,x,c,k,n,e")
+ if(a =="i")
+ for(var i =5; i<60;i++){
+     console.log(i)
+}
+ else if(a =="x")
+ for(var i =5; i<100; i++){
+     console.log(i)
+ }
+  else if(a =="c")
+ for(var i =20; i<200;i++){
+    console.log(i)
+ }
+ else if(a =="k")
+ for(var i =30; i<90;i++){
+    console.log(i%2)
+ }
+ else if(a =="n")
+ var bar=0
+ for(var i =5; i<=60;i++){
+   bar +=i
+ } 
+ console.log(bar)
+ k=1
+ for(var i = 5; i<=30; i++)
+ {
+   
+     if(i%7==0) {
+     k*=i;
+     }
+     } 
+   console.log(k)
+ else if(a =="e")
+ for(var i =5; i<30;i++){
+    console.log(i)
+   
+ }
